db2c2296d9d0c2ee17f80696dba64453bc2235d2
Mohammad <moeq24@yahoo.com>
2021-05-22 10:20:18 -0500

    initial first commit

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,7 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hi im practicing GITHUB");
         System.out.println("I got the hang of this commit and push");
-        System.out.println("Starting to be simple steps");
 
     }
 }
